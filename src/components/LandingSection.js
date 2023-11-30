4393703727529949dc6dde10f2a02c216d4b7e09
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2023-11-30 18:23:14 +0900

    cambios en el texto landinpage

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Alberto!";
-const bio1 = "An aspiring frontend developer";
-const bio2 = "learning React";
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -23,9 +23,9 @@ const LandingSection = () => (
         name="Alberto Rosell"
         src="https://i.pravatar.cc/150?img=7"
       />
-      <Heading>{greeting}</Heading>
-      <p>{bio1}</p>
-      <p>{bio2}</p>
+      <p>{greeting}</p>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
     </VStack>
   </FullScreenSection>
 );
